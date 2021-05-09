b99cc1b0f14d737c60801b53451f273178629737
Michael <83561001+michaelocez@users.noreply.github.com>
2021-05-10 09:12:20 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import sqlite3
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template(
+      'home.html',
+      title = 'Home')
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)

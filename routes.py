c26aa88f150adb48c8888c2e347b332ab0fa00ef
michaelocez <m.huangnzc@gmail.com>
2021-06-04 10:25:15 +1200

    added pictures

--- routes.py
+++ routes.py
@@ -8,14 +8,27 @@ def home():
       'home.html',
       title = 'Home')
 
+#agents route to get names from database
 @app.route('/agents/')
-def agents():
+def agent():
     conn = sqlite3.connect('12Valorant.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT name FROM Agents')
+    cursor.execute('SELECT id, name FROM Agents')
     results = cursor.fetchall()
     conn.close()
-    return render_template('agents.html', results = results)
+    return render_template('agents.html', results = results, title = 'Agents')
+
+@app.route('/agents/<int:id>')
+def agentid(id):
+    conn = sqlite3.connect('12Valorant.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Agents WHERE id = ?;'(id,))
+    agentid = cursor.fetchall()
+    conn.close()
+    return render_template('agentid.html', agentid = agentid, title = 'Agent')
+
+
+
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)

95e639373b66ebb437dad2f66b52d6aa58d666b0
michaelocez <m.huangnzc@gmail.com>
2021-06-18 10:23:28 +1200

    weapons image and logo on navbar

--- routes.py
+++ routes.py
@@ -4,11 +4,13 @@ app = Flask(__name__)
 
 #DEBUG working on do query
 def do_query(query,data= None,fetchall=False):
+    conn = sqlite3.connect('12Valorant.db')
+    cursor = conn.cursor()
     if data is None:
-        cur.execute(query)
+        cursor.execute(query)
     else:
-        cur.execute(query,data)
-    results = cur.fetchall() if fetchall else cur.fetchall()
+        cursor.execute(query,data)
+    results = cursor.fetchall() if fetchall else cur.fetchall()
 
 #home page
 @app.route('/')
@@ -20,22 +22,25 @@ def home():
 #agents route to get names from database
 @app.route('/agents/')
 def agent():
-    conn = sqlite3.connect('12Valorant.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM Agents')
-    results = cursor.fetchall()
-    conn.close()
+    results = do_query('SELECT * FROM Agents', data = None, fetchall = True)
     return render_template('agents.html', results = results, title = 'Agents')
 
 #page for each agent
 @app.route('/agents/<int:id>')
 def agentid(id):
-    conn = sqlite3.connect('12Valorant.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT Agents.*, Weapon.name FROM Agents JOIN Weapon on Agents.carrying_weapon = Weapon.id WHERE Agents.id = ?;',(id,))
-    agentid = cursor.fetchall()
-    conn.close()
+    agentid = do_query('SELECT Agents.*, Weapon.name FROM Agents JOIN Weapon on Agents.carrying_weapon = Weapon.id WHERE Agents.id = ?;',(id,), fetchall = True)
     return render_template('agentid.html', agentid = agentid, title = 'Agent')
+
+#page to get all weapons
+@app.route('/weapons/')
+def weapons():
+    weapons = do_query('SELECT * FROM Weapon', data = None, fetchall = True)
+    return render_template('weapons.html', weapons = weapons, title = 'Weapons')
+
+@app.route('/weapons/<int:id>')
+def weaponid(id):
+    agentid = do_query('SELECT * FROM Weapon', fetchall = True)
+    return render_template('weaponid.html', weaponid = weaponid, title = 'Weapon')
 
 
 if __name__ == "__main__":

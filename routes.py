25e1ff8cfecf57ff566f2bfc43e85ffb2035afd6
michaelocez <m.huangnzc@gmail.com>
2021-10-14 16:21:54 +1300

    routes.py commenting

--- routes.py
+++ routes.py
@@ -1,12 +1,15 @@
+# tells python to use flask module and other things I need such as sqlite3 for database integration
+
 import sqlite3
 from flask import Flask, render_template, request, redirect, flash, abort
 app = Flask(__name__)
 app.secret_key = '18197'
 
-#do query to avoid repeated code when using @app.route
+
+# do query to avoid repeated code when using @app.route
 def do_query(query,data= None,fetchall=False):
-    conn = sqlite3.connect('12Valorant.db')
-    cursor = conn.cursor()
+    conn = sqlite3.connect('12Valorant.db') # connects to database file
+    cursor = conn.cursor() # assigns the connections cursor
     if data is None:
         cursor.execute(query)
     else:
@@ -14,97 +17,111 @@ def do_query(query,data= None,fetchall=False):
     results = cursor.fetchall() if fetchall else cursor.fetchone()
     return results
 
-#home page
+
+# Creates a route for the flaskapp for the home route
 @app.route('/')
 def home():
-    #queries to get my favourite agent,weapon and skin
+    # queries to get my favourite agent,weapon and skin
     homea = do_query('SELECT * FROM Agents WHERE id = 11')
     homew = do_query('SELECT * FROM Weapon WHERE id = 16')
     homes = do_query('SELECT Skin.*, SkinCollection.*, Weapon.name FROM Skin JOIN SkinCollection on Skin.collection = SkinCollection.id JOIN Weapon ON Weapon.id = Skin.weapon WHERE Skin.id = 30', fetchall = True)
     return render_template('home.html', homea = homea, homew = homew, homes = homes, title = 'Home')
+    # return the render template function to the user so that they can see the html file, and add the title home to the tab
 
-#agents route to get names from database
+
+# route to get everything from agents table and present all the names and images on one page
 @app.route('/agents/')
 def agent():
     agents = do_query('SELECT * FROM Agents', data = None, fetchall = True)
     return render_template('agents.html', agents = agents, title = 'Agents')
 
-#route for page to show each agent
+
+# route to get each agents data including weapon the weapon from selected id to show on each page
 @app.route('/agents/<int:id>')
 def agentid(id):
     agentid = do_query('SELECT Agents.*, Weapon.name FROM Agents JOIN Weapon on Agents.carrying_weapon = Weapon.id WHERE Agents.id = ?;',(id,),fetchall = True)
     if len(agentid) == 0:
-        abort(404)
+        abort(404) # sending user to error page due to manual URL change
     return render_template('agentid.html', agentid = agentid, title = 'Agent')
 
-#route to show all weapons on one page
+
+# route grabs all data from weapons table and shows the image and name on page
 @app.route('/weapons/')
 def weapons():
     weapons = do_query('SELECT * FROM Weapon', data = None, fetchall = True)
     return render_template('weapons.html', weapons = weapons, title = 'Weapons')
 
-#route to show each weapon on its own page
+
+# route takes data from selected id in weapons table and presents its name, image and description of weapon
 @app.route('/weapons/<int:id>')
 def weaponid(id):
     weaponid = do_query('SELECT * FROM Weapon WHERE Weapon.id = ?',(id,), fetchall = True)
     if len(weaponid) == 0:
-        abort(404)
+        abort(404) # sending user to error page due to manual URL change
     return render_template('weaponid.html', weaponid = weaponid, title = 'Weapon')
 
-#route to show all skin collections on a page
+
+# route gets all data from skincollection table and presents all names and images on one webpage
 @app.route('/skins/')
 def skincollection():
     skincollection = do_query('SELECT * FROM SkinCollection', data = None, fetchall = True)
     return render_template('skincollection.html', skincollection = skincollection, title = 'Skins')
 
-#route to show every skin in a skin collection on its own page
+
+# route gets skin collection name, image all skins and weapons linked to selected id of skin collection
 @app.route('/skins/<int:id>')
 def skins(id):
     skins = do_query('SELECT Skin.*, SkinCollection.*, Weapon.name FROM Skin JOIN SkinCollection on Skin.collection = SkinCollection.id JOIN Weapon ON Weapon.id = Skin.weapon WHERE Skin.collection = ?',(id,), fetchall = True)
     if len(skins) == 0:
-        abort(404)
+        abort(404) # sending user to error page due to manual URL change
     return render_template('skins.html', skins = skins, title= 'Skins')
 
-#search bar to search skin collection
+
+# route for search bar to search skin collection
 @app.route('/search', methods=["POST", "GET"])
 def search():
     if request.method == "POST":
         print (request.form.get("filter"))
         search = do_query(f'SELECT * FROM SkinCollection WHERE SkinCollection.visible_name LIKE "" || ? || "%" ORDER BY SkinCollection.visible_name;', (request.form.get("filter"),), fetchall = True)
+        # query selects all from skin collection table
         if len(search) == 0:
-            abort(404)
-        elif request.form.get("filter")  =='':
-            abort(404)
+            abort(404) # if the input has 0 characters, it will abort to 404 page
+        if request.form.get("filter")  =='':
+            abort(404) # fix for when user presses enter would send to random skincollection id
         else:
-            return redirect (f'/skins/{(search[0])[0]}')
+            return redirect (f'/skins/{(search[0])[0]}') # sends user to searched page
 
-#contacts page
+
+# page with no database interaction, just displays html and css from the contact.html page
 @app.route('/contact')
 def contact():
     return render_template('contact.html', title = 'Contact')
 
-#form for user to fill in name,email and message
+
+# form for user to fill in name,email and message
 @app.route ("/message", methods=["POST"])
 def message():
-    #allows user to input their name, email and message as contact.
+    # allows user to input their name, email and message as contact.
     connection = sqlite3.connect('12Valorant.db')
     cursor = connection.cursor()
     user_first_name = request.form["user_first_name"]
     user_last_name = request.form["user_last_name"]
     user_email = request.form["user_email"]
     user_message = request.form["user_message"]
-    sql = "INSERT INTO contact(user_first_name, user_last_name, user_email, user_message) VALUES (?, ?, ?, ?)"
+    sql = "INSERT INTO contact(user_first_name, user_last_name, user_email, user_message) VALUES (?, ?, ?, ?)"  # inserts user input into contact table
     cursor.execute(sql,(user_first_name, user_last_name, user_email, user_message))
-    flash('Thank you!')
+    flash('Thank you!') # shows message after sending a message
     connection.commit()
     connection.close()
     return redirect('/contact')
 
-#error page
+
+# gives user an error when URL entered doesn't go to a route on the site
 @app.errorhandler(404)
 def error404(error):
-    return render_template('404.html', title = 'Error'), 404
+    return render_template('404.html', title = 'Error'), 404 # returns the 404 page
 
 
+# runs site on local port 8080
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
